fc470f3f4a643ef1e562d0cf8eab9b1384a5112f
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-05 16:36:36 +0100

    inserimento condizioni

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -4,5 +4,15 @@ let casualNumber = Math.floor(Math.random() * 100) + 1;
 let playerNumber;
 
 while (playerNumber = casualNumber) {
-    playerNumber = Number(prompt("Please insert a number between 1 and 100")); 
+    playerNumber = Number(prompt("Please insert a number between 1 and 100"));
+
+    // condizioni
+    if (playerNumber > casualNumber) {
+        // numero troppo alto
+        alert("The number is too high");
+    } else if (playerNumber < casualNumber) {
+        alert("The number is too low");
+    } else if (playerNumber === casualNumber) {
+        alert("You guessed the number, congratulation!");
+    }
 } 
