c5413f8b1ecc37e2f853f91bab65fdf1311c1ef9
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-05 16:13:42 +0100

    inserimento snack 1

---
+++ Snack 1/script.js
@@ -0,0 +1,13 @@
+let numbersArray = [];
+let sum = 0;
+let askNumbers;
+
+// creazione ciclo while
+
+while (sum <= 50) {
+    askNumbers = Number(prompt("Please insert number"));
+    numbersArray.push(askNumbers);
+    sum += askNumbers;
+}
+
+document.getElementById("number-sum").innerHTML = sum;
