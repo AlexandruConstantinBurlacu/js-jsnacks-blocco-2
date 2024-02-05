2114b0bf31420c1dc43fd74c5f18a9bdcaff282a
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-05 16:30:36 +0100

    prompt per chiedere il numero

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -1,3 +1,8 @@
 // generazione numero casuale da 1 a 100
 let casualNumber = Math.floor(Math.random() * 100) + 1;
 
+let playerNumber;
+
+while (playerNumber = casualNumber) {
+    playerNumber = Number(prompt("Please insert a number between 1 and 100")); 
+} 
